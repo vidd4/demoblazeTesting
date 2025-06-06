05ff894f029e67813a9530586d98a9c971c815da
vidd4 <davi.vera98@gmail.com>
2025-06-05 23:10:39 -0500

    ready

--- conftest.py
+++ conftest.py
@@ -1,13 +1,18 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import pytest
+import tempfile
+import shutil
 
 
 @pytest.fixture
 def driver():
     options = Options()
+    user_data_dir = tempfile.mkdtemp()
+    options.add_argument(f'--user-data-dir={user_data_dir}')
     options.add_argument('--headless')
     driver = webdriver.Chrome(options=options)
     driver.implicitly_wait(10)
     yield driver
     driver.quit()
+    shutil.rmtree(user_data_dir)
