6615e79c87d427e06575d2df4c0f2f90f54ba1e2
vidd4 <davi.vera98@gmail.com>
2025-06-05 23:20:31 -0500

    ready

--- conftest.py
+++ conftest.py
@@ -1,18 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import pytest
-import tempfile
-import shutil
 
 
 @pytest.fixture
 def driver():
     options = Options()
-    user_data_dir = tempfile.mkdtemp()
-    options.add_argument(f'--user-data-dir={user_data_dir}')
     options.add_argument('--headless')
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
     driver = webdriver.Chrome(options=options)
     driver.implicitly_wait(10)
     yield driver
     driver.quit()
-    shutil.rmtree(user_data_dir)
