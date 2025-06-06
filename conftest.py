4339c93a3bb02aa028276e47dc2870d0442a28db
vidd4 <davi.vera98@gmail.com>
2025-06-05 23:05:17 -0500

    ready

--- conftest.py
+++ conftest.py
@@ -1,10 +1,13 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 import pytest
 
 
 @pytest.fixture
 def driver():
-    driver = webdriver.Chrome()
+    options = Options()
+    options.add_argument('--headless')
+    driver = webdriver.Chrome(options=options)
     driver.implicitly_wait(10)
     yield driver
     driver.quit()
